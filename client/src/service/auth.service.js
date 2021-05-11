99b80811cd620406d220b1a00ef45a674be2525b
MrHose <josemiguel.uk@gmail.com>
2021-05-11 20:15:39 +0200

    minor changes

---
+++ client/src/service/auth.service.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+class AuthService {
+
+    constructor() {
+        this.app = axios.create({
+            baseURL: 'http://localhost:5000/api/auth',
+            withCredentials: true
+        })
+    }
+
+    login = userDetails => this.app.post('/login', userDetails)
+    signup = userDetails => this.app.post('/signup', userDetails)
+    logout = () => this.app.get('/logout')
+    isloggedin = () => this.app.post('/isLoggedin')
+    ispartner = () => this.app.post('/isPartner')
+    isadmin = () => this.app.post('/isAdmin')
+}
+
+export default AuthService
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,5 @@ module.exports = app => {
     app.use('/api/restaurants', require('./restaurants.routes.js'))
     app.use('/api/places', require('./places.routes.js'))
     app.use('/api/auth', require('./auth.routes.js'))
+    app.use('/api/uploads', require('./uploads.routes'))
 }
