42713c65b7985149021c6215f482794a25e2c104
MrHose <josemiguel.uk@gmail.com>
2021-05-18 10:25:36 +0200

    Events&widget

--- client/src/components/pages/events-view/Events.js
+++ client/src/components/pages/events-view/Events.js
@@ -5,7 +5,6 @@ import EventList from "./EventList";
 import Widget from "./widget";
 
 class Events extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -14,15 +13,16 @@ class Events extends Component {
     this.tmasterApp = new TmasterApp();
   }
 
-  
   componentDidMount() {
+    this.getAllEvents();
 
-    this.getAllEvents()
-    
+    const script = document.createElement("script");
+    script.src =
+      "https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js";
+    document.body.append(script);
   }
 
   getAllEvents() {
-
     const { city } = this.props.match.params;
 
     this.tmasterApp
@@ -34,10 +34,7 @@ class Events extends Component {
       .catch((err) => console.log("ERROR AL CARGAR LOS EVENTOS", err));
   }
 
-  
-
   render() {
-
     const { city } = this.props.match.params;
     const { events } = this.state;
 
@@ -53,6 +50,37 @@ class Events extends Component {
     ) : (
       <Container>
         <Widget />
+        <div
+          w-type="event-discovery"
+          w-tmapikey="oGfB9moRTPyVnKLJHowq3XANU6acUF4b"
+          w-googleapikey="AIzaSyAr9ipwI-lyXYSq3UH8SZk23md2Wt6t77M"
+          w-keyword=""
+          w-theme="simple"
+          w-colorscheme="light"
+          w-width="1115"
+          w-height="286"
+          w-size="20"
+          w-border="2"
+          w-borderradius="6"
+          w-postalcode=""
+          w-radius="25"
+          w-city=""
+          w-period="month"
+          w-layout="horizontal"
+          w-attractionid=""
+          w-promoterid=""
+          w-venueid=""
+          w-affiliateid=""
+          w-segmentid=""
+          w-proportion="custom"
+          w-titlelink="off"
+          w-sorting="groupByName"
+          w-id="id_d3bah"
+          w-countrycode="ES"
+          w-source=""
+          w-branding="Ticketmaster"
+          w-latlong=""
+        ></div>
         <h1>Proximos Eventos en {city}</h1>
         <hr />
         <Row>
